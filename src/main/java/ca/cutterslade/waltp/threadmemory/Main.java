9cd648ad0dd6852fd18f828d3b6d3fd6bdd2db17
Wesley Hartford <github@cutterslade.ca>
2014-09-09 22:18:34 -0700

    Got the basic growing mode working

--- src/main/java/ca/cutterslade/waltp/threadmemory/Main.java
+++ src/main/java/ca/cutterslade/waltp/threadmemory/Main.java
@@ -1,10 +1,12 @@
 package ca.cutterslade.waltp.threadmemory;
 
 import java.lang.management.ManagementFactory;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
 import com.lexicalscope.jewel.cli.Cli;
 import com.lexicalscope.jewel.cli.CliFactory;
 import com.lexicalscope.jewel.cli.Option;
@@ -26,16 +28,16 @@ public final class Main implements Runnable {
     @Option(defaultValue = "2048", minimum = 0, description = "Size of data block allocated by data threads")
     int getBlockSize();
 
-    @Option(defaultValue = "1024", minimum = 1, description = "Number of data blocks to allocate per thread")
+    @Option(defaultValue = "64", minimum = 1, description = "Number of data blocks to allocate per thread")
     int getBlockCount();
 
-    @Option(defaultValue = "1024", minimum = 1, description = "Number of rounds to run through")
+    @Option(defaultValue = "4", minimum = 1, description = "Number of rounds to run through")
     int getRoundCount();
 
-    @Option(defaultValue = "2000", minimum = 0, description = "Time in milliseconds to wait between alloctions")
+    @Option(defaultValue = "400", minimum = 0, description = "Time in milliseconds to wait between alloctions")
     int getThreadDelay();
 
-    @Option(defaultValue = "1000", minimum = 0, description = "Time in milliseconds to wait between reporting outputs")
+    @Option(defaultValue = "200", minimum = 0, description = "Time in milliseconds to wait between reporting outputs")
     long getReportDelay();
 
     @Option(defaultValue = "GROW", description = "The mode in which data allocating threads should operate")
@@ -91,5 +93,75 @@ public final class Main implements Runnable {
   }
 
   private void startThread() {
+    final Runnable runnable = getRunnable();
+    final Thread thread = new Thread(runnable);
+    thread.start();
+  }
+
+  private Runnable getRunnable() {
+    final Runnable runnable;
+    switch (parameters.getMode()) {
+      case GROW:
+        runnable = new GrowingThread();
+        break;
+      default:
+        throw new UnsupportedOperationException("Mode " + parameters.getMode() + " is not yet supported");
+    }
+    return runnable;
+  }
+
+  private abstract class AllocationThread implements Runnable {
+    private long threadId;
+    private long allocated;
+    private long released;
+
+    @Override
+    public void run() {
+      try {
+        synchronized (mutex) {
+          threadId = Thread.currentThread().getId();
+          doAllocation();
+          log.info("Thread {} complete", threadId);
+          finishedThreadCount--;
+        }
+      }
+      catch (final InterruptedException e) {
+        log.warn("Thread {} interrupted", threadId, e);
+      }
+    }
+
+    protected abstract void doAllocation() throws InterruptedException;
+
+    protected byte[] getBlock() {
+      allocated += parameters.getBlockSize();
+      logStats();
+      return new byte[parameters.getBlockSize()];
+    }
+
+    protected void release(final byte[] block) {
+      released += block.length;
+      logStats();
+    }
+
+    private void logStats() {
+      log.debug("Thread {} has allocated {} bytes, released {} bytes = holding {} bytes",
+          threadId, allocated, released, allocated - released);
+    }
+  }
+
+  private class GrowingThread extends AllocationThread {
+    @Override
+    protected void doAllocation() throws InterruptedException {
+      for (int round = 0; round < parameters.getRoundCount(); round++) {
+        final List<byte[]> blocks = Lists.newArrayList();
+        for (int block = 0; block < parameters.getBlockCount(); block++) {
+          blocks.add(getBlock());
+          mutex.wait(parameters.getThreadDelay());
+        }
+        for (final byte[] block : blocks) {
+          release(block);
+        }
+      }
+    }
   }
 }
